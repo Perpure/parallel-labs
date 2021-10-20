94f2e0d90613bdd6d5f0c2e92bc7c6d072ff9d0e
Paul Dyachek <dyachekp@gmail.com>
2021-10-20 19:09:15 +0300

    Scripted auto-commit on change (2021-10-20 19:09:15) by gitwatch.sh

--- lab2/src/main/java/bmstu/iu9/FlightMapper.java
+++ lab2/src/main/java/bmstu/iu9/FlightMapper.java
@@ -9,8 +9,6 @@ import java.io.IOException;
 public class FlightMapper extends Mapper<LongWritable, Text, AirportWritableComparable, FlightMapper> {
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-        ServiceCall call = new ServiceCall(value);
-        context.write(new TextPair(call.getSystemA().toString(),"1"),
-                new Text(call.toString()));
+        
     }
 }

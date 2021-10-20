da851c8e37c43dd869861dc62afd6f68a5e1d660
Paul Dyachek <dyachekp@gmail.com>
2021-10-20 19:47:07 +0300

    Scripted auto-commit on change (2021-10-20 19:47:07) by gitwatch.sh

--- lab2/src/main/java/bmstu/iu9/AirportMapper.java
+++ lab2/src/main/java/bmstu/iu9/AirportMapper.java
@@ -7,7 +7,7 @@ import org.apache.hadoop.mapreduce.Mapper;
 
 import java.io.IOException;
 
-public class AirportMapper extends Mapper<LongWritable, Text, AirportWritableComparable, IntWritable> {
+public class AirportMapper extends Mapper<LongWritable, Text, AirportWritableComparable, Text> {
     private static String DELIMITER_REGEX = "\",\"";
     private static int AIRPORT_ID_INDEX = 0;
     private static int NAME_INDEX = 1;
@@ -17,12 +17,10 @@ public class AirportMapper extends Mapper<LongWritable, Text, AirportWritableCom
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
         if (key.get() != 0) {
             String[] row = value.toString().split(DELIMITER_REGEX);
-            String delayRaw = row[DELAY_INDEX];
-            if (delayRaw.isEmpty()) return;
-            int delay = Integer.parseInt(delayRaw);
+            String nameRaw = row[NAME_INDEX].substring(1);
             int airportID = Integer.parseInt(row[AIRPORT_ID_INDEX]);
             context.write(new AirportWritableComparable(airportID, FLIGHT_TYPE),
-                    new IntWritable(delay));
+                          new IntWritable(delay));
         }
     }
 }

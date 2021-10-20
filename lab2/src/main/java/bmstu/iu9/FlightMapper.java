d042c174a2456f3d5761567010e8ac493f0c7f01
Paul Dyachek <dyachekp@gmail.com>
2021-10-20 19:16:25 +0300

    Scripted auto-commit on change (2021-10-20 19:16:25) by gitwatch.sh

--- lab2/src/main/java/bmstu/iu9/FlightMapper.java
+++ lab2/src/main/java/bmstu/iu9/FlightMapper.java
@@ -13,6 +13,6 @@ public class FlightMapper extends Mapper<LongWritable, Text, AirportWritableComp
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
         String[] row = value.toString().split(DELIMITER_REGEX);
-
+        
     }
 }

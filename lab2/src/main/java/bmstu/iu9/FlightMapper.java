fed1dd26d975fc2d4f58a98d6a2496f976875a46
Paul Dyachek <dyachekp@gmail.com>
2021-10-20 19:01:22 +0300

    Scripted auto-commit on change (2021-10-20 19:01:22) by gitwatch.sh

--- lab2/src/main/java/bmstu/iu9/FlightMapper.java
+++ lab2/src/main/java/bmstu/iu9/FlightMapper.java
@@ -2,5 +2,5 @@ package bmstu.iu9;
 
 import org.apache.hadoop.io.LongWritable;
 
-public class FlightMapper extends Mapper<LongWritable, Text,> {
+public class FlightMapper extends Mapper<LongWritable,> {
 }

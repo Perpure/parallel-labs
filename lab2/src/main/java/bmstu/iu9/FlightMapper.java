89b416a9d947c1fbb0a2841eac118df26181bd57
Paul Dyachek <dyachekp@gmail.com>
2021-10-20 19:11:28 +0300

    Scripted auto-commit on change (2021-10-20 19:11:28) by gitwatch.sh

--- lab2/src/main/java/bmstu/iu9/FlightMapper.java
+++ lab2/src/main/java/bmstu/iu9/FlightMapper.java
@@ -7,8 +7,11 @@ import org.apache.hadoop.mapreduce.Mapper;
 import java.io.IOException;
 
 public class FlightMapper extends Mapper<LongWritable, Text, AirportWritableComparable, FlightMapper> {
+
+    private static String DELIMETER_REGEX
+
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-        
+        String[] row = value.toString().split()
     }
 }

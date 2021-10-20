1470cc30ba2e7851550e04e34b9fbd70a261fdaa
Paul Dyachek <dyachekp@gmail.com>
2021-10-20 19:41:02 +0300

    Scripted auto-commit on change (2021-10-20 19:41:02) by gitwatch.sh

--- lab2/src/main/java/bmstu/iu9/AirportMapper.java
+++ lab2/src/main/java/bmstu/iu9/AirportMapper.java
@@ -1,4 +1,28 @@
 package bmstu.iu9;
 
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
 public class AirportMapper {
+    private static String DELIMITER_REGEX = ",";
+    private static int AIRPORT_ID_INDEX = 14;
+    private static int DELAY_INDEX = 17;
+    private static int FLIGHT_TYPE = 1;
+
+    @Override
+    protected void map(LongWritable key, Text value, Mapper.Context context) throws IOException, InterruptedException {
+        if (key.get() != 0) {
+            String[] row = value.toString().split(DELIMITER_REGEX);
+            String delayRaw = row[DELAY_INDEX];
+            if (delayRaw.isEmpty()) return;
+            int delay = Integer.parseInt(delayRaw);
+            int airportID = Integer.parseInt(row[AIRPORT_ID_INDEX]);
+            context.write(new AirportWritableComparable(airportID, FLIGHT_TYPE),
+                    new IntWritable(delay));
+        }
+    }
 }

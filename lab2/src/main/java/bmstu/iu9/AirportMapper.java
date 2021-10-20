6c9c895e59fbffca426b80c07740296d52ccea13
Paul Dyachek <dyachekp@gmail.com>
2021-10-20 19:52:50 +0300

    Scripted auto-commit on change (2021-10-20 19:52:50) by gitwatch.sh

--- lab2/src/main/java/bmstu/iu9/AirportMapper.java
+++ lab2/src/main/java/bmstu/iu9/AirportMapper.java
@@ -8,6 +8,7 @@ import org.apache.hadoop.mapreduce.Mapper;
 import java.io.IOException;
 
 public class AirportMapper extends Mapper<LongWritable, Text, AirportWritableComparable, Text> {
+
     private static String DELIMITER_REGEX = "\",\"";
     private static int AIRPORT_ID_INDEX = 0;
     private static int NAME_INDEX = 1;
@@ -16,13 +17,14 @@ public class AirportMapper extends Mapper<LongWritable, Text, AirportWritableCom
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
         if (key.get() != 0) {
-            String[] row = value.toString().split(DELIMITER_REGEX);
-            String airportName = row[NAME_INDEX].substring(1);
-            String airportIDRaw = row[AIRPORT_ID_INDEX];
-            airportIDRaw = airportIDRaw.substring(0, airportIDRaw.length() - 1);
-            int airportID = Integer.parseInt(airportIDRaw);
+            String rowRaw = value.toString();
+            rowRaw = rowRaw.substring(1, rowRaw.length() - 1);
+            String[] row = rowRaw.split(DELIMITER_REGEX);
+
+            String airportName = row[NAME_INDEX];
+            int airportID = Integer.parseInt(row[AIRPORT_ID_INDEX]);
             context.write(new AirportWritableComparable(airportID, AIRPORT_TYPE),
-                          new IntWritable(delay));
+                          new Text(airportName));
         }
     }
 }

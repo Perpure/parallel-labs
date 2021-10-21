f06211ec8e88cd142156df192121d557ff9d5e80
Paul Dyachek <dyachekp@gmail.com>
2021-10-21 14:53:05 +0300

    Scripted auto-commit on change (2021-10-21 14:53:05) by gitwatch.sh

--- lab2/src/main/java/bmstu/iu9/AirportReducer.java
+++ lab2/src/main/java/bmstu/iu9/AirportReducer.java
@@ -1,11 +1,17 @@
 package bmstu.iu9;
 
-public class AirportReducer Reducer<TextPair, Text, Text, Text> {
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import java.io.IOException;
+import java.util.Iterator;
+
+public class AirportReducer extends Reducer<AirportWritableComparable, Text, Text, Text> {
 
     @Override
-    protected void reduce(TextPair key, Iterable<Text> values, Context context) throws
-            IOException, InterruptedException {
-        Iterator<Text> iter = values.iterator();
+    protected void reduce(AirportWritableComparable key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+        Iterator<IntWritable> iter = values.iterator();
         Text systemInfo = new Text(iter.next());
         while (iter.hasNext()) {
             Text call = iter.next();

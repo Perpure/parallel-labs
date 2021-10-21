75db1a76c6af42aea374501171e69cbf984de8eb
Paul Dyachek <dyachekp@gmail.com>
2021-10-21 15:23:42 +0300

    Scripted auto-commit on change (2021-10-21 15:23:42) by gitwatch.sh

--- lab2/src/main/java/bmstu/iu9/AirportPartitioner.java
+++ lab2/src/main/java/bmstu/iu9/AirportPartitioner.java
@@ -1,6 +1,14 @@
 package bmstu.iu9;
 
-public class AirportPartitioner {
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Partitioner;
+
+public class AirportPartitioner extends Partitioner<AirportWritableComparable, Text> {
 
     public int get
+
+    @Override
+    public int getPartition(AirportWritableComparable airportWritableComparable, Text text, int i) {
+        return 0;
+    }
 }

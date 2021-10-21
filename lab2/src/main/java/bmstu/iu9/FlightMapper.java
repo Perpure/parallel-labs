488fc91b96804940958cb1802f2ae6d6dd77a8d5
Paul Dyachek <dyachekp@gmail.com>
2021-10-21 15:46:33 +0300

    Scripted auto-commit on change (2021-10-21 15:46:33) by gitwatch.sh

--- lab2/src/main/java/bmstu/iu9/FlightMapper.java
+++ lab2/src/main/java/bmstu/iu9/FlightMapper.java
@@ -1,6 +1,5 @@
 package bmstu.iu9;
 
-import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;

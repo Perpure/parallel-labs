5d69bba458b21f973fe9f75528a2e2df11d70a52
Paul Dyachek <dyachekp@gmail.com>
2021-10-21 15:44:42 +0300

    Scripted auto-commit on change (2021-10-21 15:44:42) by gitwatch.sh

--- lab2/src/main/java/bmstu/iu9/AirportComparator.java
+++ lab2/src/main/java/bmstu/iu9/AirportComparator.java
@@ -1,8 +1,14 @@
 package bmstu.iu9;
 
+import org.apache.hadoop.io.WritableComparable;
 import org.apache.hadoop.io.WritableComparator;
 
 public class AirportComparator extends WritableComparator {
 
-    public int compare()
+    @Override
+    public int compare(WritableComparable a, WritableComparable b) {
+        AirportWritableComparable airport1 = (AirportWritableComparable) a;
+        AirportWritableComparable airport2 = (AirportWritableComparable) b;
+        return Integer.compare(airport1.getAirportID(), airport2.getAirportID());
+    }
 }

843abbff688234abbcdb492d372f74bdd0bfd9fe
Paul Dyachek <dyachekp@gmail.com>
2021-10-21 15:15:39 +0300

    Scripted auto-commit on change (2021-10-21 15:15:39) by gitwatch.sh

--- lab2/src/main/java/bmstu/iu9/AirportReducer.java
+++ lab2/src/main/java/bmstu/iu9/AirportReducer.java
@@ -13,11 +13,16 @@ public class AirportReducer extends Reducer<AirportWritableComparable, Text, Tex
     protected void reduce(AirportWritableComparable key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
         Iterator<Text> iter = values.iterator();
         Text airportName = new Text(iter.next());
-        float min = Float.MAX_VALUE, max = Float.MIN_VALUE, sum = 0;
-        int count = 0;
+        int min = Integer.MAX_VALUE, max = Integer.MIN_VALUE, count = 0, sum = 0;
         while (iter.hasNext()) {
-            Text call = iter.next();
-            Text outValue = new Text(call.toString() + "\t" + systemInfo.toString());
+            int delay = Integer.parseInt(iter.next().toString());
+            min = Integer.min(min, delay);
+            max = Integer.max(max, delay);
+            sum += delay;
+            count++;
+        }
+        if (count > 0) {
+            Text outValue = new Text(min + ", " + max + ", " + sum / (double) count);
             context.write(airportName, outValue);
         }
     }

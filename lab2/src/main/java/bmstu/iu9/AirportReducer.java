b85476a1bc2fdadd9491e35ce9ca8214f658e596
Paul Dyachek <dyachekp@gmail.com>
2021-10-21 14:49:46 +0300

    Scripted auto-commit on change (2021-10-21 14:49:46) by gitwatch.sh

--- lab2/src/main/java/bmstu/iu9/AirportReducer.java
+++ lab2/src/main/java/bmstu/iu9/AirportReducer.java
@@ -1,4 +1,16 @@
 package bmstu.iu9;
 
 public class AirportReducer {
+
+    @Override
+    protected void reduce(TextPair key, Iterable<Text> values, Context context) throws
+            IOException, InterruptedException {
+        Iterator<Text> iter = values.iterator();
+        Text systemInfo = new Text(iter.next());
+        while (iter.hasNext()) {
+            Text call = iter.next();
+            Text outValue = new Text(call.toString() + "\t" + systemInfo.toString());
+            context.write(key.getFirst(), outValue);
+        }
+    }
 }

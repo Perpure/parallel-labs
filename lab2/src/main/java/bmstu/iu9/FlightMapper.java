170c3f5a57f4257e2383042985e1ced78dc2be43
Paul Dyachek <dyachekp@gmail.com>
2021-10-21 15:27:45 +0300

    Scripted auto-commit on change (2021-10-21 15:27:45) by gitwatch.sh

--- lab2/src/main/java/bmstu/iu9/FlightMapper.java
+++ lab2/src/main/java/bmstu/iu9/FlightMapper.java
@@ -11,7 +11,7 @@ public class FlightMapper extends Mapper<LongWritable, Text, AirportWritableComp
 
     private static String DELIMITER_REGEX = ",";
     private static int AIRPORT_ID_INDEX = 14;
-    private static int DELAY_INDEX = 17;
+    private static int DELAY_INDEX = 18;
     private static int FLIGHT_TYPE = 1;
 
     @Override
@@ -19,7 +19,7 @@ public class FlightMapper extends Mapper<LongWritable, Text, AirportWritableComp
         if (key.get() != 0) {
             String[] row = value.toString().split(DELIMITER_REGEX);
             String delayRaw = row[DELAY_INDEX];
-            if (delayRaw.isEmpty()) return;
+            if (delayRaw.isEmpty() || delayRaw.equals("0")) return;
             int airportID = Integer.parseInt(row[AIRPORT_ID_INDEX]);
             context.write(new AirportWritableComparable(airportID, FLIGHT_TYPE),
                           new Text(delayRaw));

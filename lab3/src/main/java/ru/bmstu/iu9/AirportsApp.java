57c2f288e711b81447418e76a9f721396d70cfb8
Paul Dyachek <dyachekp@gmail.com>
2021-11-21 15:01:56 +0300

    Scripted auto-commit on change (2021-11-21 15:01:56) by gitwatch.sh

--- lab3/src/main/java/ru/bmstu/iu9/AirportsApp.java
+++ lab3/src/main/java/ru/bmstu/iu9/AirportsApp.java
@@ -10,7 +10,7 @@ import java.util.Arrays;
 
 public class AirportsApp {
     public static void main(String[] args){
-        SparkConf conf = new SparkConf().setAppName("example");
+        SparkConf conf = new SparkConf().setAppName("AirportsApp");
         JavaSparkContext sc = new JavaSparkContext(conf);
         JavaRDD<String> distFile = sc.textFile("war-and-peace-1.txt");
         JavaRDD<String> splitted = distFile.flatMap(

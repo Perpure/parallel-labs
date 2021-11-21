9ada06a13f2d59b76e3d9138f3dcfb052133e622
Paul Dyachek <dyachekp@gmail.com>
2021-11-21 15:25:47 +0300

    Scripted auto-commit on change (2021-11-21 15:25:47) by gitwatch.sh

--- lab3/src/main/java/ru/bmstu/iu9/FlightsParser.java
+++ lab3/src/main/java/ru/bmstu/iu9/FlightsParser.java
@@ -1,9 +1,15 @@
 package ru.bmstu.iu9;
 
+import scala.Tuple2;
+
 public class FlightsParser {
     private static final int AIRPORT_ID_INDEX = 14;
     private static final int DELAY_INDEX = 18;
     private static final int CANCELLED_INDEX = 19;
     public static final String FLIGHT_DELIMETER = ",";
+
+    public static Tuple2<> parseFlights(String rowRaw){
+        String[] row = rowRaw.split(FLIGHT_DELIMETER);
+    }
 
 }

4000cc1c8498dfcb9b533fd17f69fce69ef38a06
Paul Dyachek <dyachekp@gmail.com>
2021-11-21 14:59:53 +0300

    Scripted auto-commit on change (2021-11-21 14:59:53) by gitwatch.sh

--- lab3/src/main/java/ru/bmstu/iu9/AirportsApp.java
+++ lab3/src/main/java/ru/bmstu/iu9/AirportsApp.java
@@ -1,7 +1,18 @@
 package ru.bmstu.iu9;
 
+import org.apache.spark.SparkConf;
+
 public class AirportsApp {
     public static void main(String[] args){
-        
+        SparkConf conf = new SparkConf(Hadoop ).setAppName(Hadoop "example");
+        JavaSparkContext sc = new JavaSparkContext(Hadoop conf);
+        JavaRDD<String> distFile = sc.textFile(Hadoop "war-and-peace-1.txt");
+        JavaRDD<String> splitted = distFile.flatMap(Hadoop
+                s -> Arrays.stream(Hadoop s.split(Hadoop " ")).iterator(Hadoop )
+        );
+        JavaPairRDD<String, Long> wordsWithCount =
+        splitted.mapToPair(Hadoop
+                s -> new Tuple2<>(Hadoop s, 1l)
+        );
     }
 }

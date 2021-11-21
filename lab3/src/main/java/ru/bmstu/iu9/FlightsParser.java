73bfd26e4ec773713dc8152f1004d08b99ed4db2
Paul Dyachek <dyachekp@gmail.com>
2021-11-21 15:24:08 +0300

    Scripted auto-commit on change (2021-11-21 15:24:08) by gitwatch.sh

--- lab3/src/main/java/ru/bmstu/iu9/FlightsParser.java
+++ lab3/src/main/java/ru/bmstu/iu9/FlightsParser.java
@@ -5,4 +5,5 @@ public class FlightsParser {
     private static final int DELAY_INDEX = 18;
     private static final int CANCELLED_INDEX = 19;
     public static final String FLIGHT_DELIMETER = ",";
+    
 }

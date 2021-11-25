a4c34afff6060c99c60ccbf6ebfd76a4acf1aa75
Paul Dyachek <dyachekp@gmail.com>
2021-11-25 14:03:30 +0300

    Scripted auto-commit on change (2021-11-25 14:03:30) by gitwatch.sh

--- lab3/src/main/java/ru/bmstu/iu9/AirportsApp.java
+++ lab3/src/main/java/ru/bmstu/iu9/AirportsApp.java
@@ -35,7 +35,7 @@ public class AirportsApp {
                     return origAirport + " -> " + destAirport + ": " + flightInfo._2.toString();
                 }
         );
-
+        infoRDD.saveAsTextFile();
     }
 
 

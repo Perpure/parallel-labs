fc0904af974000bdf9ba7d773f3a157758818bfe
Paul Dyachek <dyachekp@gmail.com>
2021-11-25 12:55:20 +0300

    Scripted auto-commit on change (2021-11-25 12:55:20) by gitwatch.sh

--- lab3/src/main/java/ru/bmstu/iu9/AirportsParser.java
+++ lab3/src/main/java/ru/bmstu/iu9/AirportsParser.java
@@ -9,7 +9,7 @@ public class AirportsParser implements Serializable {
     private static final String DELIMITER_REGEX = "\",\"";
     private static final int AIRPORT_ID_INDEX = 0;
     private static final int NAME_INDEX = 1;
-    private static final int AIRPORT_TYPE = 0;
+    private static final String HEADER_PREFIX = "CODE";
 
     private static String removeQuotes(String s) {
         return s.substring(1, s.length() - 1);
@@ -24,6 +24,6 @@ public class AirportsParser implements Serializable {
     }
 
     public static boolean IsDataRow(String row) {
-        return row.
+        return row.startsWith(HEADER_PREFIX);
     }
 }

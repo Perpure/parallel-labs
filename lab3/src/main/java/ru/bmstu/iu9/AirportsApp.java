ad00d87ee2d1ab3d16d8033345b077498a183884
Paul Dyachek <dyachekp@gmail.com>
2021-11-25 13:15:12 +0300

    Scripted auto-commit on change (2021-11-25 13:15:12) by gitwatch.sh

--- lab3/src/main/java/ru/bmstu/iu9/AirportsApp.java
+++ lab3/src/main/java/ru/bmstu/iu9/AirportsApp.java
@@ -30,7 +30,7 @@ public class AirportsApp {
         JavaRDD<String> infoRDD = flightsInfo.map(
                 flightInfo -> {
                     Map<Integer, String> airportsBroadcastedMap = airportsBroadcasted.getValue();
-                    String origAirport = airportsBroadcastedMap
+                    String origAirport = airportsBroadcastedMap.get
                 }
         )
 

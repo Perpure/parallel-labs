a02b1ba23977bc9ff1ff4571453bfd91cabf1002
Paul Dyachek <dyachekp@gmail.com>
2021-11-25 11:10:54 +0300

    Scripted auto-commit on change (2021-11-25 11:10:54) by gitwatch.sh

--- lab3/src/main/java/ru/bmstu/iu9/Flight.java
+++ lab3/src/main/java/ru/bmstu/iu9/Flight.java
@@ -5,5 +5,14 @@ import scala.Serializable;
 public class Flight implements Serializable {
     private float delay;
     private boolean isCancelled;
-    
+    private int airportOrigId;
+    private int airportDestId;
+
+
+    public Flight(float delay, boolean isCancelled, int airportOrigId, int airportDestId) {
+        this.delay = delay;
+        this.isCancelled = isCancelled;
+        this.airportOrigId = airportOrigId;
+        this.airportDestId = airportDestId;
+    }
 }

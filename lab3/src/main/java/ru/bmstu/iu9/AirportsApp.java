84966c224dd1b4f7945f6f734b183ef41f353d63
Paul Dyachek <dyachekp@gmail.com>
2021-11-25 13:03:19 +0300

    Scripted auto-commit on change (2021-11-25 13:03:19) by gitwatch.sh

--- lab3/src/main/java/ru/bmstu/iu9/AirportsApp.java
+++ lab3/src/main/java/ru/bmstu/iu9/AirportsApp.java
@@ -18,7 +18,7 @@ public class AirportsApp {
         );
 
         JavaRDD<String> airportsFile = sc.textFile("664600583_T_ONTIME_sample.csv");
-        JavaPairRDD<Integer, String> = 
+        JavaPairRDD<Integer, String> airports = airportsFile
 
 
     }

1ba2fee6f5425416378068084705b5832d82358f
Paul Dyachek <dyachekp@gmail.com>
2021-11-25 12:40:21 +0300

    Scripted auto-commit on change (2021-11-25 12:40:21) by gitwatch.sh

--- lab3/src/main/java/ru/bmstu/iu9/AirportsParser.java
+++ lab3/src/main/java/ru/bmstu/iu9/AirportsParser.java
@@ -1,7 +1,28 @@
 package ru.bmstu.iu9;
 
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
 import java.io.Serializable;
 
 public class AirportsParser implements Serializable {
-    private
+    private static final String DELIMITER_REGEX = "\",\"";
+    private static final int AIRPORT_ID_INDEX = 0;
+    private static final int NAME_INDEX = 1;
+    private static final int AIRPORT_TYPE = 0;
+
+    protected void map(LongWritable key, Text value, Mapper.Context context) throws IOException, InterruptedException {
+        if (key.get() != 0) {
+            String rowRaw = value.toString();
+            rowRaw = rowRaw.substring(1, rowRaw.length() - 1);
+            String[] row = rowRaw.split(DELIMITER_REGEX);
+
+            String airportName = row[NAME_INDEX];
+            int airportID = Integer.parseInt(row[AIRPORT_ID_INDEX]);
+            context.write(new AirportWritableComparable(airportID, AIRPORT_TYPE),
+                    new Text(airportName));
+        }
+    }
 }

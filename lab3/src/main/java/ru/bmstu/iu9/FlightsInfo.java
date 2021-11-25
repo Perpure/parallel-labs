f0e894d00dc8b2b5a70c350bf270f649dc7fdc9e
Paul Dyachek <dyachekp@gmail.com>
2021-11-25 11:24:46 +0300

    Scripted auto-commit on change (2021-11-25 11:24:46) by gitwatch.sh

--- lab3/src/main/java/ru/bmstu/iu9/FlightsInfo.java
+++ lab3/src/main/java/ru/bmstu/iu9/FlightsInfo.java
@@ -6,4 +6,10 @@ public class FlightsInfo implements Serializable {
     private int maxDelay;
     private int lateCount;
     private int cancelledCount;
+
+    public FlightsInfo(int maxDelay, int lateCount, int cancelledCount) {
+        this.maxDelay = maxDelay;
+        this.lateCount = lateCount;
+        this.cancelledCount = cancelledCount;
+    }
 }

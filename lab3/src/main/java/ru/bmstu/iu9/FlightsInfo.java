e6439be96e11ac6fe920ccd46561452b9db6e2b9
Paul Dyachek <dyachekp@gmail.com>
2021-11-25 11:39:48 +0300

    Scripted auto-commit on change (2021-11-25 11:39:48) by gitwatch.sh

--- lab3/src/main/java/ru/bmstu/iu9/FlightsInfo.java
+++ lab3/src/main/java/ru/bmstu/iu9/FlightsInfo.java
@@ -3,23 +3,26 @@ package ru.bmstu.iu9;
 import scala.Serializable;
 
 public class FlightsInfo implements Serializable {
-    private int maxDelay;
-    private int lateCount;
+    private float maxDelay;
+    private int delayedCount;
     private int cancelledCount;
     private  int totalCount;
 
-    public FlightsInfo(int maxDelay, int lateCount, int cancelledCount, int totalCount) {
+    public FlightsInfo(float maxDelay, int delayedCount, int cancelledCount, int totalCount) {
         this.maxDelay = maxDelay;
-        this.lateCount = lateCount;
+        this.delayedCount = delayedCount;
         this.cancelledCount = cancelledCount;
         this.totalCount = totalCount;
     }
 
-    public static createInfo(Flight flight) {
-        int isDelayed = flight.getDelay() > 0;
-        int
-        return new FlightsInfo(flight.getDelay(), )
+    public static FlightsInfo createInfo(Flight flight) {
+        int isDelayed = flight.getDelay() > 0 ? 1 : 0;
+        int isCancelled = flight.isCancelled() ? 1 : 0;
+        return new FlightsInfo(flight.getDelay(), isDelayed, isCancelled, 1);
     }
-    public static updateInfo
+
+    public static FlightsInfo updateInfo(FlightsInfo, Flight flight) {
+        
+    }
     public static mergeInfo
 }

0b68caae69d2bd99e36e545d3e76ed09790900ee
Paul Dyachek <dyachekp@gmail.com>
2021-11-25 13:22:18 +0300

    Scripted auto-commit on change (2021-11-25 13:22:18) by gitwatch.sh

---
+++ lab3/run.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+stop-yarn.sh
+stop-dfs.sh
+rm -rf ~/hdata/datanode
+rm -rf ~/hdata/namenode
+
+mkdir ~/hdata/datanode
+mkdir ~/hdata/namenode
+
+hdfs namenode -format
+start-dfs.sh
+start-yarn.sh
+hdfs dfs -mkdir /user
+hdfs dfs -mkdir /user/fall
+
+mvn package
+hadoop fs -copyFromLocal flights.csv
+hadoop fs -copyFromLocal airport.csv
+export HADOOP_CLASSPATH=target/Airport.jar
+hadoop bmstu.iu9.AirportMain flights.csv airport.csv output
+hadoop fs -copyToLocal output 

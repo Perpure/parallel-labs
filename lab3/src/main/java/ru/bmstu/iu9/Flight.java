bc06eac41e7e81d95b1c0f5022c76f7cbc80a541
Paul Dyachek <dyachekp@gmail.com>
2021-11-25 11:09:48 +0300

    Scripted auto-commit on change (2021-11-25 11:09:48) by gitwatch.sh

--- lab3/src/main/java/ru/bmstu/iu9/Flight.java
+++ lab3/src/main/java/ru/bmstu/iu9/Flight.java
@@ -3,5 +3,7 @@ package ru.bmstu.iu9;
 import scala.Serializable;
 
 public class Flight implements Serializable {
-    private float delay
+    private float delay;
+    private boolean isCancelled;
+    
 }

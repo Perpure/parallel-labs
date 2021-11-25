186e5bc39b485015b2981109f65c4ecc97a9aafb
Paul Dyachek <dyachekp@gmail.com>
2021-11-25 11:17:43 +0300

    Scripted auto-commit on change (2021-11-25 11:17:43) by gitwatch.sh

--- lab3/src/main/java/ru/bmstu/iu9/AirportsApp.java
+++ lab3/src/main/java/ru/bmstu/iu9/AirportsApp.java
@@ -13,7 +13,7 @@ public class AirportsApp {
         SparkConf conf = new SparkConf().setAppName("AirportsApp");
         JavaSparkContext sc = new JavaSparkContext(conf);
         JavaRDD<String> distFile = sc.textFile("664600583_T_ONTIME_sample.csv");
-        JavaPairRDD<Tuple2<Integer, Integer>, Flight>
+        JavaPairRDD<Tuple2<Integer, Integer>, Flight> = distFile.mapToPair()
         JavaRDD<String> splitted = distFile.flatMap(
                 s -> Arrays.stream(s.split(" ")).iterator()
         );

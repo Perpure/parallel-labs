ef7d4e960c3cca657655e0008dfa3bc398fb24f8
Paul Dyachek <dyachekp@gmail.com>
2021-12-03 18:37:52 +0300

    Scripted auto-commit on change (2021-12-03 18:37:52) by gitwatch.sh

--- lab4/src/main/java/ru/bmstu/iu9/ActorStore.java
+++ lab4/src/main/java/ru/bmstu/iu9/ActorStore.java
@@ -3,11 +3,18 @@ package ru.bmstu.iu9;
 import akka.actor.AbstractActor;
 import akka.japi.pf.ReceiveBuilder;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class ActorStore extends AbstractActor {
+    private Map<Integer, ArrayList<String>> testResults = new HashMap<>();
 
     @Override
     public Receive createRecieve() {
         return ReceiveBuilder.create()
     }
 
+    private void storeTestResult(StoreMessage testResult) {
+
+    }
 }

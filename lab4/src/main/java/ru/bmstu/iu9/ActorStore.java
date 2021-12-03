069b65d0b0ea6f409e03e118ce54b2e05e9b60c3
Paul Dyachek <dyachekp@gmail.com>
2021-12-03 18:49:09 +0300

    Scripted auto-commit on change (2021-12-03 18:49:09) by gitwatch.sh

--- lab4/src/main/java/ru/bmstu/iu9/ActorStore.java
+++ lab4/src/main/java/ru/bmstu/iu9/ActorStore.java
@@ -2,34 +2,36 @@ package ru.bmstu.iu9;
 
 import akka.actor.AbstractActor;
 import akka.japi.pf.ReceiveBuilder;
+import scala.Int;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 public class ActorStore extends AbstractActor {
-    private Map<Integer, ArrayList<String>> testResults = new HashMap<>();
+    private Map<Integer, ArrayList<String>> testsResults = new HashMap<>();
 
     @Override
-    public Receive createRecieve() {
+    public Receive createReceive() {
         return ReceiveBuilder.create()
                 .match(StoreMessage.class, this::storeTestResult)
                 .build();
     }
 
     private void storeTestResult(StoreMessage testResult) {
-        if (testResults.containsKey(testResult.getPackageId())) {
-            testResults.get(testResult.getPackageId()).add(testResult.getTestResult());
+        if (testsResults.containsKey(testResult.getPackageId())) {
+            testsResults.get(testResult.getPackageId()).add(testResult.getTestResult());
         } else {
             ArrayList<String> newTestList = new ArrayList<>();
             newTestList.add(testResult.getTestResult());
-            testResults.put(testResult.getPackageId(), newTestList);
+            testsResults.put(testResult.getPackageId(), newTestList);
         }
     }
 
-
-    @Override
-    public Receive createReceive() {
-        return null;
+    private void getResult(GetResultMessage query) {
+        ArrayList<String> testResults = testsResults.get(query.getPackageId());
+        sender().tell();
     }
+
+
 }

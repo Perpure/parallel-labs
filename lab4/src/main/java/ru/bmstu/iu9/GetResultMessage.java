6e4ef8a243eed9dc78b2dbc5fc68580fb39889cb
Paul Dyachek <dyachekp@gmail.com>
2021-12-03 18:34:45 +0300

    Scripted auto-commit on change (2021-12-03 18:34:45) by gitwatch.sh

--- lab4/src/main/java/ru/bmstu/iu9/GetResultMessage.java
+++ lab4/src/main/java/ru/bmstu/iu9/GetResultMessage.java
@@ -1,7 +1,21 @@
 package ru.bmstu.iu9;
 
+import java.util.ArrayList;
+
 public class GetResultMessage {
     private int packageId;
+    private ArrayList<String> testResults;
 
+    public int getPackageId() {
+        return packageId;
+    }
 
+    public ArrayList<String> getTestResults() {
+        return testResults;
+    }
+
+    public GetResultMessage(int packageId, ArrayList<String> testResults) {
+        this.packageId = packageId;
+        this.testResults = testResults;
+    }
 }

44de8f39b8da8f2d98d3c41cde4468e13ecd21ec
Paul Dyachek <dyachekp@gmail.com>
2021-12-04 00:10:18 +0300

    Scripted auto-commit on change (2021-12-04 00:10:18) by gitwatch.sh

--- lab4/src/main/java/ru/bmstu/iu9/RouterActor.java
+++ lab4/src/main/java/ru/bmstu/iu9/RouterActor.java
@@ -3,6 +3,7 @@ package ru.bmstu.iu9;
 import akka.actor.AbstractActor;
 
 public class RouterActor extends AbstractActor {
+    private static final int NUM_WORKERS = 3;
 
     @Override
     public Receive createReceive() {

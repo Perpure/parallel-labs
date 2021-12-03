0398fdf8eb59f25190df6db3acf06c09327bb080
Paul Dyachek <dyachekp@gmail.com>
2021-12-03 23:20:57 +0300

    Scripted auto-commit on change (2021-12-03 23:20:57) by gitwatch.sh

--- lab4/src/main/java/ru/bmstu/iu9/ActorRunTest.java
+++ lab4/src/main/java/ru/bmstu/iu9/ActorRunTest.java
@@ -2,13 +2,20 @@ package ru.bmstu.iu9;
 
 import akka.actor.AbstractActor;
 
+import javax.script.Invocable;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+
 public class ActorRunTest extends AbstractActor {
     @Override
     public Receive createReceive() {
         return null;
     }
 
-    private static evalScript() {
-        
+    private static String evalScript() {
+        ScriptEngine engine = new ScriptEngineManager().getEngineByName("nashorn");
+        engine.eval(jscript);
+        Invocable invocable = (Invocable) engine;
+        return invocable.invokeFunction(fun)
     }
 }

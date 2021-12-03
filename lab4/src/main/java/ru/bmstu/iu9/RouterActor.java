bd8ae570cb8f1fb89553be10320de47026ca3de4
Paul Dyachek <dyachekp@gmail.com>
2021-12-04 00:08:14 +0300

    Scripted auto-commit on change (2021-12-04 00:08:14) by gitwatch.sh

--- lab4/src/main/java/ru/bmstu/iu9/RouterActor.java
+++ lab4/src/main/java/ru/bmstu/iu9/RouterActor.java
@@ -1,5 +1,11 @@
 package ru.bmstu.iu9;
 
-public class RouterActor {
-    
+import akka.actor.AbstractActor;
+
+public class RouterActor extends AbstractActor {
+
+    @Override
+    public Receive createReceive() {
+        return null;
+    }
 }

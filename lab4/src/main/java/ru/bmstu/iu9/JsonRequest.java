0f137c0a66e84a73905634b6b4dea7e99b01e731
Paul Dyachek <dyachekp@gmail.com>
2021-12-04 08:33:12 +0300

    Scripted auto-commit on change (2021-12-04 08:33:12) by gitwatch.sh

--- lab4/src/main/java/ru/bmstu/iu9/JsonRequest.java
+++ lab4/src/main/java/ru/bmstu/iu9/JsonRequest.java
@@ -2,17 +2,49 @@ package ru.bmstu.iu9;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import java.util.ArrayList;
+
 public class JsonRequest {
-    @JsonProperty
+    @JsonProperty("packageId")
     private final String packageId;
 
-    @JsonProperty
+    @JsonProperty("jsScript")
     private final String jsScript;
 
-    @JsonProperty
+    @JsonProperty("functionName")
     private final String functionName;
 
-    @JsonProperty
-    private final String tests;
+    @JsonProperty("tests")
+    private final ArrayList<JsonTest> tests;
 
+
+    public JsonRequest(@JsonProperty("packageId") String packageId,
+                       @JsonProperty("jsScript")String jsScript,
+                       @JsonProperty("functionName") String functionName,
+                       @JsonProperty("tests") ArrayList<JsonTest> tests) {
+        this.packageId = packageId;
+        this.jsScript = jsScript;
+        this.functionName = functionName;
+        this.tests = tests;
+    }
+
+    @JsonProperty("packageId")
+    public String getPackageId() {
+        return packageId;
+    }
+
+    @JsonProperty("jsScript")
+    public String getJsScript() {
+        return jsScript;
+    }
+
+    @JsonProperty("functionName")
+    public String getFunctionName() {
+        return functionName;
+    }
+
+    @JsonProperty("tests")
+    public ArrayList<JsonTest> getTests() {
+        return tests;
+    }
 }

edc6a32b93c2f8412055de447a0819bef63c3b54
Paul Dyachek <dyachekp@gmail.com>
2021-12-04 08:16:14 +0300

    Scripted auto-commit on change (2021-12-04 08:16:14) by gitwatch.sh

--- lab4/src/main/java/ru/bmstu/iu9/ActorRouter.java
+++ lab4/src/main/java/ru/bmstu/iu9/ActorRouter.java
@@ -1,12 +1,28 @@
 package ru.bmstu.iu9;
 
 import akka.actor.AbstractActor;
+import akka.actor.ActorRef;
+import akka.actor.Props;
+import akka.routing.Routee;
 
-public class RouterActor extends AbstractActor {
+import java.util.ArrayList;
+
+public class ActorRouter extends AbstractActor {
     private static final int NUM_WORKERS = 3;
 
     @Override
     public Receive createReceive() {
         return null;
+    }
+
+    public ActorRouter() {
+        ActorRef actorStore = getContext().actorOf(Props.create(ActorStore.class));
+        getContext().watch(actorStore);
+
+        ArrayList<Routee> workers = new ArrayList<>();
+        for (int i = 0; i < NUM_WORKERS; i++) {
+            ActorRef worker = getContext().actorOf(Props.create(ActorRunTest.class));
+            
+        }
     }
 }

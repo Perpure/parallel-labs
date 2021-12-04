018bac61026a158da1807d8fa930f297783a0d0b
Paul Dyachek <dyachekp@gmail.com>
2021-12-04 08:27:09 +0300

    Scripted auto-commit on change (2021-12-04 08:27:09) by gitwatch.sh

--- lab4/src/main/java/ru/bmstu/iu9/JsonTest.java
+++ lab4/src/main/java/ru/bmstu/iu9/JsonTest.java
@@ -1,5 +1,15 @@
 package ru.bmstu.iu9;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class JsonTest {
-    
+    @JsonProperty
+    private final String ;
+
+    @JsonProperty
+    private final String packageId;
+
+    @JsonProperty
+    private final String packageId;
+
 }

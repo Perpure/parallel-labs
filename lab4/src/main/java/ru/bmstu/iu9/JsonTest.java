f33894a1ea8643d21e43188adc2fc00529fae21c
Paul Dyachek <dyachekp@gmail.com>
2021-12-04 08:30:26 +0300

    Scripted auto-commit on change (2021-12-04 08:30:26) by gitwatch.sh

--- lab4/src/main/java/ru/bmstu/iu9/JsonTest.java
+++ lab4/src/main/java/ru/bmstu/iu9/JsonTest.java
@@ -1,15 +1,43 @@
 package ru.bmstu.iu9;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.ArrayList;
 
 public class JsonTest {
     @JsonProperty("testName")
     private final String testName;
 
-    @JsonProperty
+    @JsonProperty("expectedResult")
     private final String expectedResult;
 
-    @JsonProperty
-    private final String params;
+    @JsonProperty("params")
+    private final ArrayList<Object> params;
 
+    @JsonProperty("testName")
+    public String getTestName() {
+        return testName;
+    }
+
+    @JsonProperty("expectedResult")
+    public String getExpectedResult() {
+        return expectedResult;
+    }
+
+    @JsonProperty("params")
+    public ArrayList<Object> getParams() {
+        return params;
+    }
+
+
+
+    @JsonCreator
+    public JsonTest(@JsonProperty("testName") String testName,
+                    @JsonProperty("expectedResult") String expectedResult,
+                    @JsonProperty("params") ArrayList<Object> params) {
+        this.testName = testName;
+        this.expectedResult = expectedResult;
+        this.params = params;
+    }
 }

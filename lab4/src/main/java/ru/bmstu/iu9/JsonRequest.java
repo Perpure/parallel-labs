b6c1778125f709cefb1a4e37b6fc645b4edd8788
Paul Dyachek <dyachekp@gmail.com>
2021-12-04 08:25:12 +0300

    Scripted auto-commit on change (2021-12-04 08:25:12) by gitwatch.sh

--- lab4/src/main/java/ru/bmstu/iu9/JsonRequest.java
+++ lab4/src/main/java/ru/bmstu/iu9/JsonRequest.java
@@ -1,2 +1,18 @@
-package ru.bmstu.iu9;public class JsonRequest {
+package ru.bmstu.iu9;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class JsonRequest {
+    @JsonProperty
+    private final String packageId;
+
+    @JsonProperty
+    private final String jScript;
+
+    @JsonProperty
+    private final String packageId;
+
+    @JsonProperty
+    private final String packageId;
+
 }

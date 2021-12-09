44c6c7b8517f34684f44d73b97417d997c02a02a
Paul Dyachek <dyachekp@gmail.com>
2021-12-09 14:36:34 +0300

    Scripted auto-commit on change (2021-12-09 14:36:34) by gitwatch.sh

--- lab5/src/main/java/ru/bmstu/iu9/ActorStore.java
+++ lab5/src/main/java/ru/bmstu/iu9/ActorStore.java
@@ -3,8 +3,11 @@ package ru.bmstu.iu9;
 import akka.actor.AbstractActor;
 import akka.japi.pf.ReceiveBuilder;
 
-public class ActorStore extends AbstractActor {
+import java.util.HashMap;
+import java.util.Map;
 
+public class ActorStore extends AbstractActor {
+    private Map<String, Integer> storedTimes = new HashMap<>();
 
     @Override
     public Receive createReceive() {
@@ -14,7 +17,10 @@ public class ActorStore extends AbstractActor {
     }
 
     private void getResult(GetMessage msg) {
-        
+        String url = msg.getUrl();
+        if (storedTimes.containsKey(url)) {
+            
+        }
     }
 
 }

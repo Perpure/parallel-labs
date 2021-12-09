c4828732a1897b5ead6faa0419b91e18be10e96b
Paul Dyachek <dyachekp@gmail.com>
2021-12-09 13:22:33 +0300

    Scripted auto-commit on change (2021-12-09 13:22:33) by gitwatch.sh

--- lab5/src/main/java/ru/bmstu/iu9/ActorStore.java
+++ lab5/src/main/java/ru/bmstu/iu9/ActorStore.java
@@ -1,12 +1,15 @@
 package ru.bmstu.iu9;
 
 import akka.actor.AbstractActor;
+import akka.japi.pf.ReceiveBuilder;
 
 public class ActorStore extends AbstractActor {
 
 
     @Override
     public Receive createReceive() {
-        return null;
+        return ReceiveBuilder.create()
+
+                .build();
     }
 }

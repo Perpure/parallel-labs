cb7751eda1dfcea10c8654ce143ec7f514b96749
Paul Dyachek <dyachekp@gmail.com>
2021-12-09 13:41:51 +0300

    Scripted auto-commit on change (2021-12-09 13:41:51) by gitwatch.sh

--- lab5/src/main/java/ru/bmstu/iu9/GetMessage.java
+++ lab5/src/main/java/ru/bmstu/iu9/GetMessage.java
@@ -1,5 +1,14 @@
 package ru.bmstu.iu9;
 
 public class GetMessage {
-    
+    private final String url;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public GetMessage(String url) {
+        this.url = url;
+    }
+
 }

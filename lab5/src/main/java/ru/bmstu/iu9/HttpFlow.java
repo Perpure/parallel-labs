5672eafd00edc7051581ff54b95ce14c8426d7ca
Paul Dyachek <dyachekp@gmail.com>
2021-12-16 15:17:07 +0300

    Scripted auto-commit on change (2021-12-16 15:17:07) by gitwatch.sh

--- lab5/src/main/java/ru/bmstu/iu9/HttpFlow.java
+++ lab5/src/main/java/ru/bmstu/iu9/HttpFlow.java
@@ -1,4 +1,10 @@
 package ru.bmstu.iu9;
 
+import akka.http.javadsl.model.HttpRequest;
+import akka.http.javadsl.model.HttpResponse;
+import akka.stream.javadsl.Flow;
+
 public class HttpFlow {
+
+    public static Flow<HttpRequest, HttpResponse, >
 }

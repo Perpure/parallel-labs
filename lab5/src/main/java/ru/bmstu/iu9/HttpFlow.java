bcdf677a62a81d11d990619b5d27fab68f930207
Paul Dyachek <dyachekp@gmail.com>
2021-12-16 18:53:57 +0300

    Scripted auto-commit on change (2021-12-16 18:53:57) by gitwatch.sh

--- lab5/src/main/java/ru/bmstu/iu9/HttpFlow.java
+++ lab5/src/main/java/ru/bmstu/iu9/HttpFlow.java
@@ -43,8 +43,9 @@ public class HttpFlow {
                     Duration.ofSeconds(TIMEOUT_SECS)
                 )
             .thenCompose(resultTime -> {
-
+                System.out.println(1);
                 if ((long) resultTime != -1) {
+                    System.out.println(2);
                     return CompletableFuture.completedFuture(
                             new Pair<>(
                                     request.first(),
@@ -52,7 +53,7 @@ public class HttpFlow {
                             )
                     );
                 }
-                System.out.println(1);
+                System.out.println(3);
                 Sink<Pair<String, Integer>, CompletionStage<Long>> testSink =
                         Flow.<Pair<String, Integer>>create()
                                 .mapConcat(msg ->
@@ -69,7 +70,7 @@ public class HttpFlow {
                                             .thenApply(response -> System.currentTimeMillis() - startTime);
                                 })
                                 .toMat(Sink.fold(0L, Long::sum), Keep.right());
-                System.out.println(1);
+                System.out.println(4);
                 return Source.from(Collections.singletonList(request))
                         .toMat(testSink, Keep.right())
                         .run(materializer)

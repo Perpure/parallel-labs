55874114e797b920d1fc60d8495dd79686eb65fc
Paul Dyachek <dyachekp@gmail.com>
2021-12-16 21:47:08 +0300

    Scripted auto-commit on change (2021-12-16 21:47:08) by gitwatch.sh

--- lab6/src/main/java/ru/bmstu/iu9/AkkaMain.java
+++ lab6/src/main/java/ru/bmstu/iu9/AkkaMain.java
@@ -1,4 +1,56 @@
 package ru.bmstu.iu9;
 
-public class AkkaMain {
+
+import akka.Done;
+import akka.NotUsed;
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.actor.Props;
+import akka.http.javadsl.ConnectHttp;
+import akka.http.javadsl.Http;
+import akka.http.javadsl.ServerBinding;
+import akka.http.javadsl.marshallers.jackson.Jackson;
+import akka.http.javadsl.model.HttpRequest;
+import akka.http.javadsl.model.HttpResponse;
+import akka.http.javadsl.model.StatusCodes;
+import akka.http.javadsl.server.AllDirectives;
+import akka.http.javadsl.server.Route;
+import akka.pattern.Patterns;
+import akka.stream.ActorMaterializer;
+import akka.stream.javadsl.Flow;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
+import java.util.concurrent.Future;
+
+// localhost:8080?testUrl=http://ya.ru&count=20
+
+
+public class AkkaMain extends AllDirectives {
+
+
+    public static void main(String[] args) throws Exception {
+        // boot up server using the route as defined below
+        ActorSystem system = ActorSystem.create("TestingSystem");
+        ActorRef actorStore = system.actorOf(Props.create(ActorStore.class));
+
+        final Http http = Http.get(system);
+        final ActorMaterializer materializer = ActorMaterializer.create(system);
+
+        //In order to access all directives we need an instance where the routes are define.
+        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = HttpFlow.httpFlow(materializer, actorStore);
+        final CompletionStage<ServerBinding> binding = http.bindAndHandle(routeFlow,
+                ConnectHttp.toHost("localhost", 8080), materializer);
+
+        System.out.println("Server online at http://localhost:8080/\nPress RETURN to stop...");
+        System.in.read(); // let it run until user presses return
+
+        binding
+                .thenCompose(ServerBinding::unbind) // trigger unbinding from the port
+                .thenAccept(unbound -> system.terminate()); // and shutdown when done
+    }
 }

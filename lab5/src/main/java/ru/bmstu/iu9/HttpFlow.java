ec733ed684bd90cf87b4522fb46e1671eda386d2
Paul Dyachek <dyachekp@gmail.com>
2021-12-16 15:19:22 +0300

    Scripted auto-commit on change (2021-12-16 15:19:22) by gitwatch.sh

--- lab5/src/main/java/ru/bmstu/iu9/HttpFlow.java
+++ lab5/src/main/java/ru/bmstu/iu9/HttpFlow.java
@@ -1,11 +1,15 @@
 package ru.bmstu.iu9;
 
 import akka.NotUsed;
+import akka.actor.ActorRef;
 import akka.http.javadsl.model.HttpRequest;
 import akka.http.javadsl.model.HttpResponse;
+import akka.stream.ActorMaterializer;
 import akka.stream.javadsl.Flow;
 
 public class HttpFlow {
 
-    public static Flow<HttpRequest, HttpResponse, NotUsed> 
+    public static Flow<HttpRequest, HttpResponse, NotUsed> httpFlow(
+            ActorMaterializer materializer, ActorRef actor
+    )
 }

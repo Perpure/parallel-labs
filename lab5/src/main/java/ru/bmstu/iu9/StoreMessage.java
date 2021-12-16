30423c5539c78dc251ea3a6b5addc5abc31a4834
Paul Dyachek <dyachekp@gmail.com>
2021-12-16 15:02:26 +0300

    Scripted auto-commit on change (2021-12-16 15:02:26) by gitwatch.sh

--- lab5/src/main/java/ru/bmstu/iu9/StoreMessage.java
+++ lab5/src/main/java/ru/bmstu/iu9/StoreMessage.java
@@ -1,5 +1,20 @@
 package ru.bmstu.iu9;
 
 public class StoreMessage {
-    private final String url
+    private final String url;
+    private final Integer time;
+
+    public StoreMessage(String url, Integer time) {
+        this.url = url;
+        this.time = time;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public Integer getTime() {
+        return time;
+    }
+
 }

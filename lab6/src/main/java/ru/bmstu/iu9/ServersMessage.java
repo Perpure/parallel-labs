a0e09bd6e9e037f6236dcb3ac81676ff8d3d4ada
Paul Dyachek <dyachekp@gmail.com>
2021-12-17 13:55:07 +0300

    Scripted auto-commit on change (2021-12-17 13:55:07) by gitwatch.sh

--- lab6/src/main/java/ru/bmstu/iu9/ServersMessage.java
+++ lab6/src/main/java/ru/bmstu/iu9/ServersMessage.java
@@ -1,4 +1,15 @@
 package ru.bmstu.iu9;
 
+import java.util.ArrayList;
+
 public class ServersMessage {
+    private final ArrayList<String> servers;
+
+    public ArrayList<String> getServers() {
+        return servers;
+    }
+
+    public ServersMessage(ArrayList<String> servers) {
+        this.servers = servers;
+    }
 }

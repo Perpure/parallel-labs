fe75abec6682964d858cf00c6a5813d7430fa954
Paul Dyachek <dyachekp@gmail.com>
2021-12-17 14:34:44 +0300

    Scripted auto-commit on change (2021-12-17 14:34:44) by gitwatch.sh

--- lab6/src/main/java/ru/bmstu/iu9/ZooKeeperInstance.java
+++ lab6/src/main/java/ru/bmstu/iu9/ZooKeeperInstance.java
@@ -1,8 +1,23 @@
 package ru.bmstu.iu9;
 
+import org.apache.zookeeper.*;
+
+import java.io.IOException;
+import java.util.List;
+
 public class ZooKeeperInstance {
 
-    public ZooKeeperInstance() {
-        
+    public ZooKeeperInstance() throws IOException, InterruptedException, KeeperException {
+        ZooKeeper zoo = new ZooKeeper("127.0.0.1:2181", 3000, this);
+        zoo.create("/servers/s",
+                "data".getBytes(),
+                ZooDefs.Ids.OPEN_ACL_UNSAFE ,
+                CreateMode.EPHEMERAL_SEQUENTIAL
+);
+        List<String> servers = zoo.getChildren("/servers", (Watcher) this);
+        for (String s : servers) {
+            byte[] data = zoo.getData("/servers/" + s, false, null);
+            System.out.println("server " + s + " data=" + new String(data));
+        }
     }
 }

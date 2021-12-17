1bf6b4a1fd44deddd2bd5784d5c5e0dd4c66445d
Paul Dyachek <dyachekp@gmail.com>
2021-12-17 15:01:02 +0300

    Scripted auto-commit on change (2021-12-17 15:01:02) by gitwatch.sh

--- lab6/src/main/java/ru/bmstu/iu9/AnonymousRouter.java
+++ lab6/src/main/java/ru/bmstu/iu9/AnonymousRouter.java
@@ -2,6 +2,7 @@ package ru.bmstu.iu9;
 
 import akka.actor.ActorRef;
 import akka.http.javadsl.Http;
+import akka.http.javadsl.server.Route;
 
 public class AnonymousRouter {
     private final ActorRef actorConfig;
@@ -12,6 +13,21 @@ public class AnonymousRouter {
         this.httpClient = httpClient;
     }
 
+    public Route createRoute() {
 
+        return route(
+                get(() -> parameter("packageId", (id) -> {
+                    Future<Object> response = Patterns.ask(
+                            router,
+                            new GetStoredMessage(id),
+                            Timeout.create(Duration.ofSeconds(15)));
+                    return completeOKWithFuture(response, Jackson.marshaller());
+                })),
+                post(() -> entity(Jackson.unmarshaller(JsonRequest.class), (jsonRequest) -> {
+                    router.tell(jsonRequest, ActorRef.noSender());
+                    return complete("Request received");
+                }))
+        );
+    }
 
 }

1a7ca5a2ce95dff58e1f41e5fb6b7b26d50ca1b6
Paul Dyachek <dyachekp@gmail.com>
2021-12-17 15:18:02 +0300

    Scripted auto-commit on change (2021-12-17 15:18:02) by gitwatch.sh

--- lab6/src/main/java/ru/bmstu/iu9/AnonymousRouter.java
+++ lab6/src/main/java/ru/bmstu/iu9/AnonymousRouter.java
@@ -5,7 +5,9 @@ import akka.http.javadsl.Http;
 import akka.http.javadsl.model.HttpRequest;
 import akka.http.javadsl.model.HttpResponse;
 import akka.http.javadsl.server.Route;
+import akka.pattern.Patterns;
 
+import java.time.Duration;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionStage;
 
@@ -14,6 +16,7 @@ import static akka.http.javadsl.server.Directives.*;
 public class AnonymousRouter {
     private final ActorRef actorConfig;
     private final Http httpClient;
+    private final int TIMEOUT_SECS = 10;
 
     public AnonymousRouter(ActorRef actorConfig, Http httpClient) {
         this.actorConfig = actorConfig;
@@ -34,6 +37,12 @@ public class AnonymousRouter {
         );
     }
 
-    private CompletionStage<HttpResponse> 
+    private CompletionStage<HttpResponse> sendToRandomServer(String url, int count) {
+        return Patterns.ask(actorConfig, new EmptyMessage(), Duration.ofSeconds(TIMEOUT_SECS))
+                .thenCompose(server -> {
+                    String query = server +
+                            "url"
+                })
+    }
 
 }

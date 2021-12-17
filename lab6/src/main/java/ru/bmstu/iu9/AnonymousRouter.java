2c34204b85cf8bb1200a630c253ebd3d73fed464
Paul Dyachek <dyachekp@gmail.com>
2021-12-17 14:59:04 +0300

    Scripted auto-commit on change (2021-12-17 14:59:03) by gitwatch.sh

--- lab6/src/main/java/ru/bmstu/iu9/AnonymousRouter.java
+++ lab6/src/main/java/ru/bmstu/iu9/AnonymousRouter.java
@@ -1,13 +1,15 @@
 package ru.bmstu.iu9;
 
 import akka.actor.ActorRef;
+import akka.http.javadsl.Http;
 
 public class AnonymousRouter {
     private final ActorRef actorConfig;
+    private final Http httpClient;
 
     public AnonymousRouter(ActorRef actorConfig) {
         this.actorConfig = actorConfig;
     }
 
-    
+
 }
